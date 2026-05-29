3e41f9bf124ce81059b60a34ba88b1e1c87b05a3
Anamika Sharma <112106640+Anamika-29@users.noreply.github.com>
2026-05-29 17:45:46 +0530

    Time: 1 ms (85.97%), Space: 55.4 MB (34.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,17 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   
-    let map = {};
-
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-
-        if (map.hasOwnProperty(complement)) {
-            return [map[complement], i];
-        }
-
-        map[nums[i]] = i;
+    let map = new Map()
+    for(let i = 0;i<nums.length;i++){
+        let remainder = target - nums[i];
+        if(map.has(remainder)){
+            return [map.get(remainder),i]
+        };
+        map.set(nums[i], i);
     }
 
 };
